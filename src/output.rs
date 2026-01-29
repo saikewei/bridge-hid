0457cdf3710919185b34e28cd38f48b5b9732fdb
saikewei <huangyicheng0302@qq.com>
2026-01-29 23:33:26 +0800

    feat: bluetooth hid keyboard and mouse

--- src/output.rs
+++ src/output.rs
@@ -1,3 +1,4 @@
+pub mod bluetooth;
 pub mod usb;
 
 use anyhow::Result;
@@ -260,5 +261,7 @@ pub mod keycodes {
 }
 
 // 重新导出常用类型
+pub use bluetooth::BluetoothKeyboardHidDevice;
+pub use bluetooth::BluetoothMouseHidDevice;
 pub use usb::UsbKeyboardHidDevice;
 pub use usb::UsbMouseHidDevice;
