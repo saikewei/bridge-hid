79365ef901bd8293a0107f3517a4ecd0c12b49d8
saikewei <huangyicheng0302@qq.com>
2026-02-04 03:19:26 +0800

    chore: use multi-thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bridge_hid::core;
 use bridge_hid::logging::init;
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() -> anyhow::Result<()> {
     init();
     let core = core::Core::new();
