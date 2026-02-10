6d9ef52826d6f4eac222e603a1562d98971b0026
saikewei <huangyicheng0302@qq.com>
2026-02-10 14:35:44 +0800

    feat: introduce clap

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
 use bridge_hid::core;
 use bridge_hid::logging::init;
+use clap::{Parser, ValueEnum};
+use log::{debug, info};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// 运行模式: switcher | web-touchpad
+    #[arg(long, value_enum, default_value = "switcher")]
+    mode: Mode,
+}
+
+#[derive(Copy, Clone, Debug, ValueEnum)]
+enum Mode {
+    Switcher,
+    WebTouchpad,
+}
+
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() -> anyhow::Result<()> {
     init();
+    let args = Args::parse();
+
+    debug!("启动模式: {:?}", args.mode);
+    match args.mode {
+        Mode::Switcher => run_switcher().await?,
+        Mode::WebTouchpad => run_web_touchpad().await?,
+    }
+    Ok(())
+}
+
+async fn run_switcher() -> anyhow::Result<()> {
     let core = core::Core::new();
     core.run().await?;
+
+    Ok(())
+}
+
+async fn run_web_touchpad() -> anyhow::Result<()> {
+    info!("Web Touchpad mode is not yet implemented.");
     Ok(())
 }
